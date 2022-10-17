8bbcb2cd5811e542281280ccdbb1a43a9c7b3373
Pawel Burgchardt <pawel.burgchardt@gmail.com>
2022-10-17 16:13:35 -0500

    changed init msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Super <code>Chrumka</code>
         </p>
         <a
           className="App-link"
